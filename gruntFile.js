3f5d1abe67020c10d32d6b6f436b60cbe7c5f78a
Amos <amos@shallwefootball.com>
2015-08-24 21:01:04 +0900

    add shell script, iterator
    그런데 key값으로 하나씩 넣을 순 있지만 key값이 정말 많아지면 iter를 돌려야 할 방법을 찾아야 한다.

--- gruntFile.js
+++ gruntFile.js
@@ -4,7 +4,7 @@ module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     webfont: {
-      icons: {
+      amos: {
         src: 'sample_svg/svg16/*.svg',  // file 16개
         // src: 'sample_svg/svg50/*.svg',  // file 50개
         // src: 'sample_svg/svg180/*.svg',  // file 180개
@@ -14,6 +14,7 @@ module.exports = function (grunt) {
         options: {
           //default: 'fontforge' should install fontforge
           // engine: 'node',
+          stylesheet: 'sass',
 
            // if (ture) Append font file names with unique string to flush browser cache when you update your icons.
           hashes: false,
@@ -23,10 +24,31 @@ module.exports = function (grunt) {
 
           destHtml: './dest/svg16fontforge/'
         }
+      },
+      amos2: {
+        // src: 'sample_svg/svg16/*.svg',  // file 16개
+        src: 'sample_svg/svg50/*.svg',  // file 50개
+        // src: 'sample_svg/svg180/*.svg',  // file 180개
+        // src: 'sample_svg/svg251/*.svg',  // file 251개
+        dest: 'dest/svg50fontforge/font',
+        destCss: 'dest/svg50fontforge/css',
+        options: {
+          //default: 'fontforge' should install fontforge
+          // engine: 'node',
+          stylesheet: 'sass',
+
+           // if (ture) Append font file names with unique string to flush browser cache when you update your icons.
+          hashes: false,
+
+          // if convert to woff2, read this 'https://github.com/sapegin/grunt-webfont/wiki/WOFF2-support'
+          types: ['eot', 'woff', 'woff2', 'ttf', 'svg'],
+
+          destHtml: './dest/svg50fontforge/'
+        }
       }
     },
 
-    clean: ['dest/svg16fontforge/*']
+    clean: ['dest/svg50fontforge/*']
   })
 
   grunt.loadNpmTasks('grunt-webfont');
